04e048c3c253adb1082c6b28b840b1b49c3f2d0b
Justin <jrt0614@tamu.edu>
2025-09-25 17:00:13 -0500

    Started Event and Rehearsal page

---
+++ spec/views/events/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "events/index", type: :view do
+  before(:each) do
+    assign(:events, [
+      Event.create!(
+        title: "Title",
+        location: "Location"
+      ),
+      Event.create!(
+        title: "Title",
+        location: "Location"
+      )
+    ])
+  end
+
+  it "renders a list of events" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Location".to_s), count: 2
+  end
+end
