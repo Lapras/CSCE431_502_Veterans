c1d20d41481d8544dddd173aed696f352d67951d
d4kk4 <jermurray2@gmail.com>
2025-09-25 18:55:00 -0500

    Pluralizing DB migration and fixing RSPEC

--- db/migrate/20250925234500_add_admin_users_for_google_oauth.rb
+++ db/migrate/20250925234500_add_admin_users_for_google_oauth.rb
@@ -1,19 +1,21 @@
 class AddAdminUsersForGoogleOauth < ActiveRecord::Migration[7.2]
+  ADMIN_EMAILS = [
+    'jermurray2@tamu.edu',
+  ]
   def up
-    # Replace with your Gmail OAuth email
-    email = 'jermurray2@tamu.edu'
 
-    # Only create if it doesn't already exist
-    user = User.find_or_initialize_by(email: email)
-    
-    user.save!
+    ADMIN_EMAILS.each do |email|
+      user = User.find_or_create_by!(email: email) do |u|
+      end
 
-    user.add_role :admin unless user.has_role?(:admin)
+      user.add_role(:admin) unless user.has_role?(:admin)
+    end
   end
 
   def down
-    email = 'jermurray2@tamu.edu'
-    user = User.find_by(email: email)
-    user.destroy if user
+    ADMIN_EMAILS.each do |email|
+      user = User.find_by(email: email)
+      user.remove_role(:admin) if user&.has_role?(:admin)
+    end
   end
 end
