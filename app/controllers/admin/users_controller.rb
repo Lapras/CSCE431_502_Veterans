6a33c77acf7eeb2f0778ed07fbced25b09c87649
Ryan Mohammadian <ryanm@MacBook-Pro-318.local>
2025-09-27 22:16:11 -0500

    Fixes to pass Rubocop and Brakeman

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,55 +1,57 @@
-class Admin::UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :require_admin!
-  before_action :set_user, only: %i[ show edit update destroy ]
+# frozen_string_literal: true
 
-  # GET /admin/users or /admin/users.json
-  def index
-    @users = User.includes(:roles).order(:full_name, :email)
-  end
+module Admin
+  class UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_admin!
+    before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /admin/users/1 or /admin/users/1.json
-  def show
-  end
-
-  # GET /admin/users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /admin/users/1/edit
-  def edit
-  end
-
-  # POST /admin/users or /admin/users.json
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      update_roles(@user)
-      redirect_to [:admin, @user], notice: "User created"
-    else
-      render :new, status: :unprocessable_entity
+    # GET /admin/users or /admin/users.json
+    def index
+      @users = User.includes(:roles).order(:full_name, :email)
     end
-  end
 
-  # PATCH/PUT /admin/users/1 or /admin/users/1.json
-  def update
-    if @user.update(user_params)
-      update_roles(@user)
-      redirect_to [:admin, @user], notice: "User updated"
-    else
-      render :edit, status: :unprocessable_entity
+    # GET /admin/users/1 or /admin/users/1.json
+    def show; end
+
+    # GET /admin/users/new
+    def new
+      @user = User.new
     end
-  end
 
-  # DELETE /admin/users/1 or /admin/users/1.json
-  def destroy
-    @user.destroy!
+    # GET /admin/users/1/edit
+    def edit; end
 
-    redirect_to admin_users_path, notice: "User deleted"
-  end
+    # POST /admin/users or /admin/users.json
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        update_roles(@user)
+        redirect_to [:admin, @user], notice: t('admin.users.created')
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 
-  private
+    # PATCH/PUT /admin/users/1 or /admin/users/1.json
+    def update
+      if @user.update(user_params)
+        update_roles(@user)
+        redirect_to [:admin, @user], notice: t('admin.users.updated')
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /admin/users/1 or /admin/users/1.json
+    def destroy
+      @user.destroy!
+
+      redirect_to admin_users_path, notice: t('admin.users.deleted')
+    end
+
+    private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
@@ -61,13 +63,14 @@ class Admin::UsersController < ApplicationController
     end
 
     def update_roles(user)
-      return unless params.dig(:user, :role_names).present?
+      return if params.dig(:user, :role_names).blank?
+
       user.roles = []
-      params[:user][:role_names].reject(&:blank?).each {|r| user.add_role(r)}
+      params[:user][:role_names].compact_blank.each { |r| user.add_role(r) }
     end
 
     def require_admin!
-      redirect_to root_path, alert: "Unauthorized" unless current_user&.has_role?(:admin)
+      redirect_to root_path, alert: t('admin.users.unauthorized') unless current_user&.has_role?(:admin)
     end
-
+  end
 end
