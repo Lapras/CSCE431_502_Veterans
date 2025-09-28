3cf0ed67f2dbce5e0cdc9b2934f62384bb9c272e
d4kk4 <jermurray2@gmail.com>
2025-09-27 21:29:09 -0500

    Linting results and various linting stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "not_a_member", to: "static_pages#not_a_member", as: :not_a_member
+  get 'not_a_member', to: 'static_pages#not_a_member', as: :not_a_member
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
