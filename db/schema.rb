e4e6dcb463803eae715872c67cac3f47bfd854d2
d4kk4 <jermurray2@gmail.com>
2025-09-29 17:05:45 -0500

    Schema automated update from Ruby

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_26_202033) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_29_181619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "starts_at"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
@@ -40,12 +48,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_26_202033) do
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
-  end
-  create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.datetime "starts_at"
-    t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 end
