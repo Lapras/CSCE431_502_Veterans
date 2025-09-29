7981f39a0047ce562c159c9b2472a1a992a12d00
d4kk4 <jermurray2@gmail.com>
2025-09-29 16:40:52 -0500

    Rubocop changes

--- db/migrate/20250925234500_add_admin_users_for_google_oauth.rb
+++ db/migrate/20250925234500_add_admin_users_for_google_oauth.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require "securerandom"
+
+require 'securerandom'
 
 class AddAdminUsersForGoogleOauth < ActiveRecord::Migration[7.2]
   ADMIN_EMAILS = [
@@ -7,11 +8,11 @@ class AddAdminUsersForGoogleOauth < ActiveRecord::Migration[7.2]
   ].freeze
   def up
     ADMIN_EMAILS.each do |email|
-      user = User.find_or_create_by!(email: email)
+      User.find_or_create_by!(email: email)
       user = User.find_or_initialize_by(email: email)
-      user.full_name ||= "Seed Admin"
+      user.full_name ||= 'Seed Admin'
       user.uid       ||= SecureRandom.uuid
-      user.avatar_url ||= ""
+      user.avatar_url ||= ''
 
       user.save!(validate: false)
 
@@ -23,7 +24,7 @@ class AddAdminUsersForGoogleOauth < ActiveRecord::Migration[7.2]
     ADMIN_EMAILS.each do |email|
       user = User.find_by(email: email)
       next unless user
-      
+
       user.remove_role(:admin) if user&.has_role?(:admin)
     end
   end
