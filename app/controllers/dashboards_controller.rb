77bb687f5adde5d11d156d5da573d75dc89ca9e1
d4kk4 <jermurray2@gmail.com>
2025-09-29 21:17:10 -0500

    Rubocop autofixes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,14 +3,14 @@
 class DashboardsController < ApplicationController
   layout 'user'
   before_action :authenticate_user!
-  
+
   def show
     # Redirect admins to admin dashboard
     if current_user.has_role?(:admin)
       redirect_to admin_dashboard_path
       return
     end
-    
+
     # User-specific data
     @user = current_user
   end
