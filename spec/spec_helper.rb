14f8f8ab906ea476078e84441eea87c2a9bfae2e
d4kk4 <jermurray2@gmail.com>
2025-10-07 17:11:39 -0500

    Edting simplecov so we aren't covering pre-built files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,21 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   minimum_coverage 90  # fails if overall coverage < 90%
   minimum_coverage_by_file 85
+
+  # Exclude Rails auto-generated files
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/config/'
+  add_filter '/spec/'   
+  add_filter '/vendor/'
+  add_filter '/app/channels/application_cable/'
+  add_filter 'app/mailers/application_mailer.rb'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/models/ability.rb'
+
+  #Skipping auth tests
+  add_filter 'app/controllers/users/sessions_controller.rb'
+  add_filter 'app/controllers/users/omniauth_callbacks_controller.rb'
 end
 
 
