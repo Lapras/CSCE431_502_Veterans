cdba4de8adc15e62f5a324f5a51914e371a0219a
alleny2017 <alleny2017@tamu.edu>
2025-10-16 09:12:18 -0500

    remove file that caused db:migrate error

--- db/migrate/20251014221704_add_event_id_to_excusal_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddEventIdToExcusalRequests < ActiveRecord::Migration[7.2]
-  def change
-    add_reference :excusal_requests, :event, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_14_221704) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_14_214216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,27 +23,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_14_221704) do
   end
 
   create_table "excusal_requests", force: :cascade do |t|
-    t.text "reason"
-    t.datetime "starts_at"
-    t.datetime "ends_at"
     t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.text "reason"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "event_id", null: false
     t.index ["event_id"], name: "index_excusal_requests_on_event_id"
     t.index ["user_id"], name: "index_excusal_requests_on_user_id"
-  end
-
-  create_table "excusals", force: :cascade do |t|
-    t.text "reason"
-    t.string "status"
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_excusals_on_event_id"
-    t.index ["user_id"], name: "index_excusals_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -76,6 +63,4 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_14_221704) do
 
   add_foreign_key "excusal_requests", "events"
   add_foreign_key "excusal_requests", "users"
-  add_foreign_key "excusals", "events"
-  add_foreign_key "excusals", "users"
 end
