aaca82fe1e637b38c30dff5933ee273e67307170
d4kk4 <jermurray2@gmail.com>
2025-10-16 16:22:58 -0500

    aaand we're trying filenames again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-SimpleCov.start 'rails' do
-  minimum_coverage 90  # fails if overall coverage < 90%
-  minimum_coverage_by_file 85
+if File.basename($0) =~ /rspec$/
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    minimum_coverage 90  # fails if overall coverage < 90%
+    minimum_coverage_by_file 85
 
-  # Exclude Rails auto-generated files
-  add_filter '/bin/'
-  add_filter '/db/'
-  add_filter '/config/'
-  add_filter '/spec/'   
-  add_filter '/vendor/'
-  add_filter '/app/channels/application_cable/'
-  add_filter 'app/mailers/application_mailer.rb'
-  add_filter 'app/jobs/application_job.rb'
-  add_filter 'app/models/ability.rb'
+    # Exclude Rails auto-generated files
+    add_filter '/bin/'
+    add_filter '/db/'
+    add_filter '/config/'
+    add_filter '/spec/'   
+    add_filter '/vendor/'
+    add_filter '/app/channels/application_cable/'
+    add_filter 'app/mailers/application_mailer.rb'
+    add_filter 'app/jobs/application_job.rb'
+    add_filter 'app/models/ability.rb'
+  end
 end
 
 
