c449cd758e37886bc2e4f38368b5ff40c55068ae
Ryan Mohammadian <ryanm@MacBook-Pro-318.local>
2025-10-18 15:11:57 -0500

    Adjusted database issues

--- db/migrate/20251018064048_add_checked_in_at_to_attendances.rb
+++
@@ -1,5 +0,0 @@
-class AddCheckedInAtToAttendances < ActiveRecord::Migration[7.2]
-  def change
-    add_column :attendances, :checked_in_at, :datetime unless column_exists?(:attendances, :checked_in_at)
-  end
-end
