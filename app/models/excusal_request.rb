0e8fc20b2e77d5a94f412631176901efc90c9971
alleny2017 <alleny2017@tamu.edu>
2025-10-17 21:44:28 -0500

    removee recurring excusal conditionals from excusal request model

--- app/models/excusal_request.rb
+++ app/models/excusal_request.rb
@@ -1,15 +1,7 @@
 class ExcusalRequest < ApplicationRecord
   belongs_to :user
-  belongs_to :event, optional: true   
-  serialize :recurring_days, Array   
+  belongs_to :event
   validates :reason, presence: true
   validates :user_id, presence: true
-  validates :event_id, presence: true, unless: -> { recurring }
-  validate :validate_recurring_fields, if: -> { recurring }
-  private
-  def validate_recurring_fields
-    if recurring_days.blank? || recurring_start_time.blank? || recurring_end_time.blank?
-      errors.add(:base, "Recurring days and time range must be present for recurring excusals")
-    end
-  end
+  validates :event_id, presence: true
 end
