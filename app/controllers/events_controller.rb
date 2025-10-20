a11f86c8dc75d13a7d518768e1f1f7b941e4abb4
d4kk4 <jermurray2@gmail.com>
2025-10-20 17:10:09 -0500

    Adding notes to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -83,7 +83,7 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.require(:event).permit(:title, :starts_at, :location, user_ids: [])
+    params.require(:event).permit(:title, :starts_at, :location, :notes, user_ids: [])
   end
 
   def require_admin!
