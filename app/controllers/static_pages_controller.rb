b1c8e7af98aff053088c354aef55eac01b579d25
d4kk4 <jermurray2@gmail.com>
2025-10-19 23:25:11 -0500

    Adding the static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,11 @@
 class StaticPagesController < ApplicationController
   skip_before_action :check_user_roles, only: [:not_a_member]
   def not_a_member; end
+
+  def documentation_and_support
+    # Only allow users who are NOT :not_a_member
+    if current_user.roles.empty? || current_user.has_role?(:not_a_member)
+      redirect_to root_path, alert: "You are not authorized to view this page."
+    end
+  end
 end
