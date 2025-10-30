9340d8fc54dfc9cfb318c57d32543df8ea08870a
d4kk4 <jermurray2@gmail.com>
2025-10-30 16:57:09 -0500

    Rubocop linting

--- db/migrate/20251028012248_add_evidence_link_to_excusal_requests.rb
+++ db/migrate/20251028012248_add_evidence_link_to_excusal_requests.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEvidenceLinkToExcusalRequests < ActiveRecord::Migration[7.2]
   def change
     add_column :excusal_requests, :evidence_link, :string
--- db/migrate/20251028012426_add_evidence_link_to_reccuring_excusal_requests.rb
+++ db/migrate/20251028012426_add_evidence_link_to_reccuring_excusal_requests.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEvidenceLinkToReccuringExcusalRequests < ActiveRecord::Migration[7.2]
   def change
     add_column :recurring_excusals, :evidence_link, :string
