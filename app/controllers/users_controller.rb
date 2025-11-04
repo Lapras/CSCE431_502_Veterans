13c3ff5c6b87cd342acf74e6be1a6a24d7d79ec5
Ryan Mohammadian <ryanm@MacBook-Pro-318.local>
2025-11-04 15:10:51 -0600

    Added functionality for all types of users and added side panel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  layout :select_layout
 
   def profile
     @user = current_user
+  end
+
+  private
+
+  def select_layout
+    current_user&.has_role?(:admin) ? 'admin' : 'user'
   end
 end
