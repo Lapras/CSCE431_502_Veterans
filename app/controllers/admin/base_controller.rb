857e472b266804d365ebddf7d7a4b7f550227670
d4kk4 <jermurray2@gmail.com>
2025-11-07 09:58:59 -0600

    Removing duplicate user auth

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,6 @@ module Admin
   class BaseController < ApplicationController
     before_action -> { require_role!(:admin, :officer) }
 
-    before_action :authenticate_user!
     load_and_authorize_resource
 
     layout 'admin'
